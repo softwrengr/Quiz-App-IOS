24600684190933791b0713136a30bf2081f5bb59
eapple <softwrengr@gmail.com>
2020-09-04 05:23:22 +0400

    Initial Commit

---
+++ Quiz App IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz App IOS
+//
+//  Created by EAPPLE on 04/09/2020.
+//  Copyright © 2020 Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
